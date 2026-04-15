7bcd6b9b2561e86474d79e891a20f7851fd9395b
Salov Zahar <sosikoni@gmail.com>
2026-04-15 19:09:41 +0300

    remove unnecessary files

--- ml/test.py
+++
@@ -1,6 +0,0 @@
-from sources import find_sources
-from inference import classify_text
-from deep_analysis import deep_analyse
-
-promt = "Internet restrictions, arms control: daily briefing from Kremlin. Dmitry Peskov said that Russia is currently forced to impose digital restrictions for security reasons"
-
